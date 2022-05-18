50607488f83bb3b949736c0dc1fec1334eab8fd4
mblanc2414 <mblanc2414@gmail.com>
2022-05-18 12:11:54 -0400

    Fruits D3 Final Commit

--- server.js
+++ server.js
@@ -11,7 +11,13 @@ const port = 3000;
 
 
 // mount middleware
+app.use((req, res, next) => {
+    console.log("I run for all routes")
+    next()
+  })
 
+//near the top, around other app.use() calls
+app.use(express.urlencoded({ extended: false }))
 // mount routes
 
 // Index
@@ -19,8 +25,9 @@ app.get('/fruits', (req, res) => {
     res.render('index.ejs', { allFruits: fruits });
                             // ^-- this is just a descriptive way of referencing our fruits array inside index.ejs
 });
+
 // LINES 23-32 BELOW....7 Different Routes for rendering information New. Delete. Update. Create. Edit. Show
-// N
+// NEW
 app.get("/fruits/new", (req, res) => {
 res.render("new.ejs")
 })
@@ -28,11 +35,15 @@ res.render("new.ejs")
 
 // U
 
-// C
+// CREATE
 
+app.post("/fruits", (req, res) => {
+    fruits.push(req.body)
+    res.redirect('/fruits')
+  })
 // E
 
-// Show
+// SHOW
 app.get('/fruits/:indexOfFruitsArray', (req, res) => {
     // render is a special method that
     // informs the template engine to render a template
